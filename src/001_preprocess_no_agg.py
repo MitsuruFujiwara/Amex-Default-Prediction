279930f55dfcbd5d68050de7bbbc3805e0c6e10a
MitsuruFujiwara <fujiwara52jp@gmail.com>
2022-07-05 10:57:15 +0000

    add day & month & year to target encoding

--- src/001_preprocess_no_agg.py
+++ src/001_preprocess_no_agg.py
@@ -24,13 +24,6 @@ def main():
     # merge target
     train_df = train_df.merge(train_labels,how='left',on='customer_ID')
 
-    # target encoding
-    print('target encoding...')
-    for c in tqdm(CAT_COLS):
-        dict_target = train_df[[c,'target']].groupby(c).mean()['target']
-        train_df[f'{c}_te'] = train_df[c].map(dict_target)
-        test_df[f'{c}_te'] = test_df[c].map(dict_target)
-
     # add is test flag
     train_df['is_test'] = False
     test_df['is_test'] = True
@@ -43,18 +36,31 @@ def main():
     train_df['days_diff'] = train_df[['customer_ID','S_2']].groupby('customer_ID').diff()['S_2'].dt.days
     test_df['days_diff'] = test_df[['customer_ID','S_2']].groupby('customer_ID').diff()['S_2'].dt.days
 
+    # datetime features
+    train_df['day'] = train_df['S_2'].dt.day
+    train_df['month'] = train_df['S_2'].dt.month
+    train_df['year'] = train_df['S_2'].dt.year
+    train_df['seasonality_m'] = np.cos(np.pi*(train_df['S_2'].dt.day/31*2-1))
+    train_df['seasonality_y'] = np.cos(np.pi*(train_df['S_2'].dt.dayofyear/366*2-1))
+
+    test_df['day'] = test_df['S_2'].dt.day
+    test_df['month'] = test_df['S_2'].dt.month
+    test_df['year'] = test_df['S_2'].dt.year
+    test_df['seasonality_m'] = np.cos(np.pi*(test_df['S_2'].dt.day/31*2-1))
+    test_df['seasonality_y'] = np.cos(np.pi*(train_df['S_2'].dt.dayofyear/366*2-1))
+
+    # target encoding
+    print('target encoding...')
+    for c in tqdm(CAT_COLS+['day','month','year']):
+        dict_target = train_df[[c,'target']].groupby(c).mean()['target']
+        train_df[f'{c}_te'] = train_df[c].map(dict_target)
+        test_df[f'{c}_te'] = test_df[c].map(dict_target)
+
     # merge train & test
     df = pd.concat([train_df,test_df])
 
     del train_df, test_df
     gc.collect()
-
-    # datetime features
-    df['day'] = df['S_2'].dt.day
-    df['month'] = df['S_2'].dt.month
-    df['year'] = df['S_2'].dt.year
-    df['seasonality_m'] = np.cos(np.pi*(df['S_2'].dt.day/31*2-1))
-    df['seasonality_y'] = np.cos(np.pi*(df['S_2'].dt.dayofyear/366*2-1))
 
     # drop unnecessary columns
     df.drop('S_2',axis=1,inplace=True)
