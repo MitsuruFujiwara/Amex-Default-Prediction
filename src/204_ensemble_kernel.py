196200b5350ffcf204f1d4480a781d6cef632b8d
MitsuruFujiwara <fujiwara52jp@gmail.com>
2022-08-13 23:20:15 +0000

    sub#41

--- src/204_ensemble_kernel.py
+++ src/204_ensemble_kernel.py
@@ -33,7 +33,7 @@ def main():
     sub_adhithyasrinivasan['prediction'] = sub_adhithyasrinivasan['prediction'].rank() / len(sub_adhithyasrinivasan)
 
     # calc prediction
-    sub['prediction'] += 0.3*sub_seed_avg['prediction']+0.7*sub_adhithyasrinivasan['prediction']
+    sub['prediction'] += 0.5*sub_seed_avg['prediction']+0.5*sub_adhithyasrinivasan['prediction']
 
     # to rank
     sub['prediction'] = sub['prediction'].rank() / len(sub)
